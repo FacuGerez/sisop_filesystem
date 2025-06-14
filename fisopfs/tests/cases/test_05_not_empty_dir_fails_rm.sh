0ec9111a3d8c875b9e22670dcbb10ad49aa1a1b5
Taielmolina01 <tamolina@fi.uba.ar>
2025-06-14 11:57:51 -0300

    adding fix for test 5; deleting test 12 and comment in informe

--- fisopfs/tests/cases/test_05_not_empty_dir_fails_rm.sh
+++ fisopfs/tests/cases/test_05_not_empty_dir_fails_rm.sh
@@ -4,6 +4,6 @@ MOUNT=tests/mount
 
 mkdir "$MOUNT"/testdir
 touch "$MOUNT"/testdir/testfile
-rmdir "$MOUNT"/testdir
+rmdir "$MOUNT"/testdir  2>&1
 ls "$MOUNT"
 ls "$MOUNT"/testdir
--- fisopfs/tests/cases/test_12_err_redirection_works.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-MOUNT=tests/mount
-
-cat "$MOUNT"/moms_spaghetti 2> "$MOUNT"/errorfile
-ls "$MOUNT"
-cat "$MOUNT"/errorfile
